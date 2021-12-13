b68aae6e48401c990e33e8adc858936f5499a3e7
Riafka <Riafka.sir@yandex.ru>
2021-12-13 12:12:20 +0500

    HW9 + optional

--- src/main/java/ru/javawebinar/topjava/to/MealTo.java
+++ src/main/java/ru/javawebinar/topjava/to/MealTo.java
@@ -1,21 +1,36 @@
 package ru.javawebinar.topjava.to;
 
+import org.hibernate.validator.constraints.Range;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.beans.ConstructorProperties;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
 public class MealTo extends BaseTo {
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME, fallbackPatterns = {"yyyy-MM-dd HH:mm"})
+    @NotNull(message = "must be not empty")
+    private LocalDateTime dateTime;
 
-    private final LocalDateTime dateTime;
+    @NotBlank
+    @Size(min = 2, max = 120)
+    private String description;
 
-    private final String description;
+    @Range(min = 10, max = 5000)
+    @NotNull(message = "must be not empty")
+    private Integer calories;
 
-    private final int calories;
+    private boolean excess;
 
-    private final boolean excess;
+    public MealTo() {
+
+    }
 
     @ConstructorProperties({"id", "dateTime", "description", "calories", "excess"})
-    public MealTo(Integer id, LocalDateTime dateTime, String description, int calories, boolean excess) {
+    public MealTo(Integer id, LocalDateTime dateTime, String description, Integer calories, boolean excess) {
         super(id);
         this.dateTime = dateTime;
         this.description = description;
@@ -39,16 +54,33 @@ public class MealTo extends BaseTo {
         return excess;
     }
 
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCalories(Integer calories) {
+        this.calories = calories;
+    }
+
+    public void setExcess(boolean excess) {
+        this.excess = excess;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         MealTo mealTo = (MealTo) o;
-        return calories == mealTo.calories &&
+        return
                 excess == mealTo.excess &&
-                Objects.equals(id, mealTo.id) &&
-                Objects.equals(dateTime, mealTo.dateTime) &&
-                Objects.equals(description, mealTo.description);
+                        Objects.equals(calories, mealTo.calories) &&
+                        Objects.equals(id, mealTo.id) &&
+                        Objects.equals(dateTime, mealTo.dateTime) &&
+                        Objects.equals(description, mealTo.description);
     }
 
     @Override
--- src/main/webapp/resources/js/topjava.meals.js
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -20,6 +20,10 @@ function clearFilter() {
 $(function () {
     makeEditable(
         $("#datatable").DataTable({
+            "ajax": {
+                "url": mealAjaxUrl,
+                "dataSrc": ""
+            },
             "paging": false,
             "info": true,
             "columns": [
@@ -34,11 +38,13 @@ $(function () {
                 },
                 {
                     "defaultContent": "Edit",
-                    "orderable": false
+                    "orderable": false,
+                    "render": renderEditBtn
                 },
                 {
                     "defaultContent": "Delete",
-                    "orderable": false
+                    "orderable": false,
+                    "render": renderDeleteBtn
                 }
             ],
             "order": [
@@ -46,7 +52,75 @@ $(function () {
                     0,
                     "desc"
                 ]
-            ]
+            ],
+            "createdRow": function (row, data, dataIndex) {
+                $(row).attr("data-meal-excess", data.excess);
+            }
         })
     );
+});
+
+$.ajaxSetup({
+    converters: {
+        "text json": function (stringData) {
+            var json = JSON.parse(stringData);
+            if (typeof json === 'object') {
+                $(json).each(function () {
+                    if (this.hasOwnProperty('dateTime')) {
+                        this.dateTime = this.dateTime.substr(0, 16).replace('T', ' ');
+                    }
+                });
+            }
+            return json;
+        }
+    }
+});
+$.datetimepicker.setLocale(window.navigator.language.substring(0, 2));
+
+//  http://xdsoft.net/jqplugins/datetimepicker/
+var startDate = $('#startDate');
+var endDate = $('#endDate');
+startDate.datetimepicker({
+    timepicker: false,
+    format: 'Y-m-d',
+    formatDate: 'Y-m-d',
+    onShow: function (ct) {
+        this.setOptions({
+            maxDate: endDate.val() ? endDate.val() : false
+        })
+    }
+});
+endDate.datetimepicker({
+    timepicker: false,
+    format: 'Y-m-d',
+    formatDate: 'Y-m-d',
+    onShow: function (ct) {
+        this.setOptions({
+            minDate: startDate.val() ? startDate.val() : false
+        })
+    }
+});
+
+var startTime = $('#startTime');
+var endTime = $('#endTime');
+startTime.datetimepicker({
+    datepicker: false,
+    format: 'H:i',
+    onShow: function (ct) {
+        this.setOptions({
+            maxTime: endTime.val() ? endTime.val() : false
+        })
+    }
+});
+endTime.datetimepicker({
+    datepicker: false,
+    format: 'H:i',
+    onShow: function (ct) {
+        this.setOptions({
+            minTime: startTime.val() ? startTime.val() : false
+        })
+    }
+});
+$('#dateTime').datetimepicker({
+    format: 'Y-m-d H:i'
 });
